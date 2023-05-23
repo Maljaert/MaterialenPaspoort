9f6edd8d6fca7d6137b96f5d0b26e94ee3f43160
Maljaert <134371443+Maljaert@users.noreply.github.com>
2023-05-23 09:52:24 +0200

    Update GetMaterialsandCount.py

--- GetMaterials.py
+++ GetMaterials.py
@@ -5,12 +5,25 @@ from ifcopenshell.util.element import get_elements_by_material as ele_by_materia
 ifc_file = ifcopenshell.open('test.ifc')
 print(ifc_file.schema)
 
-direct_materials = ifc_file.by_type('IfcMaterial')
-# Print the name of each direct material
+# Store unique material names and their instance counts
+material_info = {}
+
+direct_materials = ifc_file.by_type('IfcMaterialConstituent')
+
+# Print the name of each direct material and its instance count
 for material in direct_materials:
     if material.Name:
-        print(material.Name)
+        material_name = material.Name
+        if material_name not in material_info:
+            material_info[material_name] = 1
+        else:
+            material_info[material_name] += 1
+        print(material_name)
 
     for ele in ele_by_materials(ifc_file=ifc_file, material=material):
-        print('|-->',ele.Name, '(', ele.is_a(), ')')
+        print('|-->', ele.Name, '(', ele.is_a(), ')')
 
+# Print the instance count for each material
+print("\nMaterial Instance Counts:")
+for material_name, count in material_info.items():
+    print(material_name, ":", count)
